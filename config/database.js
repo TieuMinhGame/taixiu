ddc1dda545762b665a0c7eb34c4d08f393fc0836
TieuMinhGame <minhreuspro123@gmail.com>
2021-10-30 21:26:04 +0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+	'url': 'mongodb+srv://admin:@123456@cluster0.9h5fe.mongodb.net/server?retryWrites=true&w=majority',
+	'options': {
+		//'user':   'root',
+		//'pass':   '',
+		'dbName': 'server',
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
